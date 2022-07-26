a0e5ccca84d7fe5310081c49809b9308b4be3953
hanyiyu <46866888+hanyiyu@users.noreply.github.com>
2022-07-26 15:39:43 -0700

    change homepage

---
+++ screens/homepage/CardView.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Text, View , Image} from 'react-native'
+import Card from '../../components/Card';
+
+
+const styles = StyleSheet.create({
+  Container: {
+   
+  },
+ 
+  image: {
+    borderRadius: 25,
+    resizeMode: "contain",
+    height: 50,
+    width: 50,
+    marginRight:10,
+  },
+});
+
+type Card = {
+  username:string;
+  image:string;
+  date:string;
+  content:string;
+}
+type CardProps = {
+  card:Card;
+}
+
+const CardView = (props : CardProps) => {
+    const {username,image,date,content} = props.card;
+    console.log(image);
+    const cardDom = 
+      <View style={styles.Container}>
+      <View style={{ flexDirection:'row',paddingBottom:20}}>
+        <Image style = {styles.image}
+               source={{uri:image}} />
+
+        <View>
+          <Text style ={{ fontSize: 18,
+                fontWeight:'bold',
+                marginBottom:10,
+          }}>{username}</Text>
+          <Text style ={{
+                color: 'lightgray',
+                fontWeight:'bold',
+          }}>{date}</Text>
+        </View>
+    </View>
+    <Text style ={{
+      fontSize:20,
+      marginBottom:20
+    }}>{content}</Text>
+
+    </View>;
+
+    return (
+      <>
+       <Card children={cardDom}></Card>
+      </>
+    )
+  
+}
+
+export default CardView
+
+      
+     
--- screens/homepage/HomepageScreen.tsx
+++ screens/homepage/HomepageScreen.tsx
@@ -1,19 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
-import Button from '../../components/Button'
+import { StyleSheet, Text, View , FlatList,ListRenderItem} from 'react-native'
+import CardView from './CardView'
 import React from 'react'
+import Card from '../../components/Card';
 
 type Props = {}
 
+type Card = {
+      username:string;
+      image:string;
+      date:string;
+      content:string;
+}
+
+// static uri, should be changed in different localhost
+const staticImage = "/Users/hanyiyu/Documents/code/tietie_mobile/screens/homepage/images/icon-192x192.png";
+
+const cardValue : Card[]= [
+
+      {username:"tietie用户",image:staticImage,date:"Sun Jun 26 2022",content:"这课真有意思啊"},
+      {username:"tietie用户",image:staticImage,date:"Sun Jun 26 2022",content:"这课真有意思啊"},
+      {username:"tietie用户",image:staticImage,date:"Sun Jun 26 2022",content:"这课真有意思啊"},
+      {username:"tietie用户",image:staticImage,date:"Sun Jun 26 2022",content:"这课真有意思啊"},
+      {username:"tietie用户",image:staticImage,date:"Sun Jun 26 2022",content:"这课真有意思啊"},
+];
+
+
+
+
 const HomepageScreen = (props: Props) => {
       return (
-            <View>
-                  <Button
-                        onPress={() => {
-
-                        }}
-                        title="Button"
-                  />
-            </View>
+            <>
+               <FlatList 
+                numColumns={2}
+                data={cardValue}
+                renderItem={ ({item }:{item:Card}) => ( 
+                
+                   <View style={{
+                        width:'50%',
+                        paddingTop:5,
+                        paddingLeft:3,
+                        paddingRight:3
+                  }}>
+                        <CardView card={item}/>
+                  </View>
+                 
+                 
+                  
+                )}
+               >   
+               </FlatList>
+            </>
       )
 }
 
