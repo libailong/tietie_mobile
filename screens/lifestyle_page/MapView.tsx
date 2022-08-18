852d15d902f9ee32843ca78f68873d2d6183fc1f
Wenze Ma <maw1@rose-hulman.edu>
2022-08-17 21:24:18 -0500

    new search bar in lifestyle section

---
+++ screens/lifestyle_page/MapView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const MapView = () => {
+    return (
+        <View>
+            <Text>Map View</Text>
+        </View>
+    );
+};
+
+export default MapView;
