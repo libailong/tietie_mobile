c0ef6550a26ac6b8fd0e34171ff758d0acde77f3
Ran Xu <Ran Xu>
2022-08-24 20:09:24 -0500

    re-implemented tabbar using rneui

--- screens/referral_page/ReferralScreen.tsx
+++ screens/referral_page/ReferralScreen.tsx
@@ -1,16 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Card from '../../components/Card';
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import * as themes from '../../constants/themes';
-import HorizontalDivider from '../../components/HorizontalDivider';
-import UserInfoHeader from '../../components/UserInfoHeader';
+import React, { useState } from 'react'
 import PostCard from './components/PostCard';
+import Tabbar from '../../components/Tabbar';
+import TabView from '../../components/TabView';
 
 type Props = {}
 
 const ReferralScreen = (props: Props) => {
-      return (
+      const [index, setIndex] = useState(0);
+      const amazonDummyPage = (
             <>
                   <PostCard
                         title='2023 NG SDE'
@@ -27,6 +25,66 @@ const ReferralScreen = (props: Props) => {
                         desc='A new position at Amazon'
                         username='user_10000'
                         postTime='Jun 26, 2022'
+                  />
+            </>
+      )
+      const googleDummyPage = (
+            <>
+                  <PostCard
+                        title='2023 NG SDE'
+                        company='Google'
+                        location='Seattle-WA'
+                        desc='A new position at Amazon'
+                        username='user_10000'
+                        postTime='Jun 26, 2022'
+                  />
+                  <PostCard
+                        title='2023 NG SDE'
+                        company='Google'
+                        location='Seattle-WA'
+                        desc='A new position at Amazon'
+                        username='user_10000'
+                        postTime='Jun 26, 2022'
+                  />
+            </>
+      )
+      const tiktokDummyPage = (
+            <>
+                  <PostCard
+                        title='2023 NG SDE'
+                        company='TikTok'
+                        location='Seattle-WA'
+                        desc='A new position at Amazon'
+                        username='user_10000'
+                        postTime='Jun 26, 2022'
+                  />
+                  <PostCard
+                        title='2023 NG SDE'
+                        company='TikTok'
+                        location='Seattle-WA'
+                        desc='A new position at Amazon'
+                        username='user_10000'
+                        postTime='Jun 26, 2022'
+                  />
+            </>
+      )
+      const pages = [amazonDummyPage, googleDummyPage, tiktokDummyPage];
+      return (
+            <>
+
+                  <Tabbar
+                        data={{
+                              "Amazon": "amazon",
+                              "Google": "google",
+                              "TikTok": "TikTok"
+                        }}
+                        onValueChanged={(value) => { setIndex(value) }}
+                        label={index}
+                  />
+                  <TabView
+                        pages={pages}
+                        index={index}
+                        setIndex={setIndex}
                   />
             </>
       )
