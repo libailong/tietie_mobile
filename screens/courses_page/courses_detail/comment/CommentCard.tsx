7dca6fa1bcb7f18c3f5a7a2a083773639eba5309
hanyiyu <46866888+hanyiyu@users.noreply.github.com>
2022-09-05 23:29:04 -0700

    add details

--- screens/courses_page/courses_detail/comment/CommentCard.tsx
+++ screens/courses_page/courses_detail/comment/CommentCard.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View ,ImageSourcePropType} from 'react-native'
 import React from 'react'
 import UserInfoHeader from '../../../../components/UserInfoHeader'
+import Card from '../../../../components/Card'
 
 type Props = {
     info:CommentInfo,
@@ -16,10 +17,10 @@ export type CommentInfo = {
 const CommentCard = (props: Props) => {
       
       return (
-           <>
+           <Card>
              <UserInfoHeader username={props.info.username} subHeadline={props.info.subHeadline}></UserInfoHeader>
              <Text>{props.info.commentText}</Text>
-           </>
+           </Card>
         )
 }
 
--- screens/courses_page/courses_detail/courseInfo/index.tsx
+++ screens/courses_page/courses_detail/courseInfo/index.tsx
@@ -1,86 +1,119 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../../../../components/Card';
-import Button from '../../../../components/Button';
+import {Button} from '@rneui/themed';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type Props = {
-  data: CourseInfoData;
+    data: CourseInfoData;
 };
 
 export type CourseInfoData = {
-  coursename: string;
-  professor: string;
-  semester: string;
-  timespend: number;
-  difficulty: number;
-  coursecomment: string;
+    coursename: string;
+    professor: string;
+    semester: string;
+    timespend: number;
+    difficulty: number;
+    coursecomment: string;
 };
 
 const CourseInfo = (props: Props) => {
-  const {coursename, professor, semester, timespend, difficulty,coursecomment} = props.data;
+    const {
+        coursename,
+        professor,
+        semester,
+        timespend,
+        difficulty,
+        coursecomment,
+    } = props.data;
+    const [agree_value, set_agree_value] = useState(0);
+    const [disagree_value, set_disagree_value] = useState(0);
 
-  const agree = () => {};
-  const disagree = () => {};
-  const cardDom = (
-    <>
-      <View style={styles.textGroup}>
-        <Text style={styles.title}>Course Name:</Text>
-        <Text style={styles.textContent}>{coursename}</Text>
-      </View>
-      <View style={styles.textGroup}>
-        <Text style={styles.title}>Professor:</Text>
-        <Text style={styles.textContent}>{professor}</Text>
-      </View>
-      <View style={styles.textGroup}>
-        <Text style={styles.title}>Semester:</Text>
-        <Text style={styles.textContent}>{semester}</Text>
-      </View>
-      <View style={styles.textGroup}>
-        <Text style={styles.title}>Time Spent per Week:</Text>
-        <Text style={styles.textContent}>{timespend}</Text>
-      </View>
-      <View style={styles.textGroup}>
-        <Text style={styles.title}>Difficulty:</Text>
-        <Text style={styles.textContent}>{difficulty}</Text>
-      </View>
+    const cardDom = (
+        <>
+            <View style={styles.textGroup}>
+                <Text style={styles.title}>Course Name:</Text>
+                <Text style={styles.textContent}>{coursename}</Text>
+            </View>
+            <View style={styles.textGroup}>
+                <Text style={styles.title}>Professor:</Text>
+                <Text style={styles.textContent}>{professor}</Text>
+            </View>
+            <View style={styles.textGroup}>
+                <Text style={styles.title}>Semester:</Text>
+                <Text style={styles.textContent}>{semester}</Text>
+            </View>
+            <View style={styles.textGroup}>
+                <Text style={styles.title}>Time Spent per Week:</Text>
+                <Text style={styles.textContent}>{timespend}</Text>
+            </View>
+            <View style={styles.textGroup}>
+                <Text style={styles.title}>Difficulty:</Text>
+                <Text style={styles.textContent}>{difficulty}</Text>
+            </View>
 
-      <View style={styles.buttonGroup}>
-        <Button onPress={agree}></Button>
-        <Button onPress={disagree}></Button>
-      </View>
-    </>
-  );
+            <View style={styles.buttonGroup}>
+                <Button
+                    size="sm"
+                    type="outline"
+                    onPress={() => {
+                        set_agree_value(agree_value + 1);
+                    }}
+                    buttonStyle={styles.button}>
+                    <Icon name="caret-up" size={20} color="#000000" />
+                    <Text>{agree_value}</Text>
+                </Button>
+                <Button
+                    size="sm"
+                    type="outline"
+                    onPress={() => {
+                        set_disagree_value(disagree_value + 1);
+                    }}
+                    buttonStyle={styles.button}>
+                    <Icon name="caret-down" size={20} color="#000000" />
+                    <Text>{disagree_value}</Text>
+                </Button>
+            </View>
+        </>
+    );
 
-  return (
-    <View>
-      <Card children={cardDom}></Card>
-      <Text style={styles.title}>How Does It Feel?</Text>
-      <Text style={styles.textContent}>{coursecomment}</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Card children={cardDom}></Card>
+            <View style={styles.feelContent}>
+               <Text style={styles.title}>How Does It Feel?</Text>
+               <Text style={styles.textContent}>{coursecomment}</Text>
+            </View>
+           
+        </View>
+    );
 };
 
 export default CourseInfo;
 
 const styles = StyleSheet.create({
-  card: {},
-  buttonGroup: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  button: {
-    padding: 10,
-  },
-  textGroup:{
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 18,
-  },
-  textContent:{
-    fontSize: 18,
-  }
+    card: {},
+    buttonGroup: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    button: {
+        marginLeft: 10,
+    },
+    textGroup: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+       
+    },
+    textContent: {      
+        fontSize: 18,
+    },
+    feelContent:{
+      padding:10
+    }
 });
